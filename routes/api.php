10de3e21344a0a8c8ce5bceed9bc78801a4fc501
faizalrifki <faizalrifkiarlans@gmail.com>
2021-10-24 22:23:30 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::resource('me', 'App\Http\Controllers\AuthController');
+Route::resource('books', BookController::class)->except('create', 'edit');
